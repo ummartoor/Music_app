1446889fbb01ab2b958eedca8aaf11c4e5ae38b0
ummartoor <toorummar@gmail.com>
2022-10-10 06:19:10 +0500

    Music App Project

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+import { MdTitle } from "react-icons/md";
+
+const Error = () => (
+  <div className="w-full flex justify-center items-center">
+    <h1 className="font-bold text-2xl text-white mt-2">
+      Something went wrong. Please try again.
+    </h1>
+  </div>
+);
+
+export default Error;
---
+++ src/redux/services/ShazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "1c7b6889a6msh1cc9c911d12b577p18da64jsn9d48408a76b0"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
